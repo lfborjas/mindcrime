5498600069a3c9b4637510d2a3cfa4d7f52ecef6
Luis Borjas <luisfborjas@gmail.com>
2012-11-11 00:42:28 -0600

    Renders slides and adds task for auto-compiling
    Uses [handlebars](http://handlebarsjs.com) directly for rendering and
    was aided by this [article on clojurescript and
    node](http://mmcgrana.github.com/2011/09/clojurescript-nodejs.html)
    Also, uses a convenience function to cast cljs objects to javascript
    objects (to be able to use them on the handlebars templates), thanks to
    this project:
    <https://github.com/ibdknox/jayq/blob/master/src/jayq/util.cljs>
    
    Also, to speed up development, added the `dev` task to the Makefile,
    which will fire [cljs-watch](https://github.com/ibdknox/cljs-watch) to
    re-compile files every time they change

--- Makefile
+++ Makefile
@@ -1,4 +1,7 @@
 js:
 	cljsc src '{:optimizations :simple :target :nodejs :pretty-print true :output-dir "lib" :output-to "lib/main.js"}'
 
+dev:
+	cljs-watch src '{:optimizations :simple :target :nodejs :pretty-print true :output-dir "lib" :output-to "lib/main.js"}'
+
 .PHONY: js
--- src/web.cljs
+++ src/web.cljs
@@ -1,20 +1,70 @@
 ;based on http://gist.github.com/1172019
 
 (ns mindcrime
-  (:require [cljs.nodejs :as node]))
+  (:require 
+    [cljs.nodejs :as node]))
 
 (def express (node/require "express"))
+(def fs      (node/require "fs"))
+(def engine  (node/require "handlebars"))
 
 (def app (express))
 
-(defn hello-world [request response]
-  (.send response "Hello clojurescript"))
+(defn slurp [filename]
+  (str (. fs (readFileSync filename))))
+
+;from https://github.com/ibdknox/jayq/blob/master/src/jayq/util.cljs
+(defn clj->js [x]
+  (cond
+    (string? x) x
+    (keyword? x) (name x)
+    (map? x) (let [obj (js-obj)]
+               (doseq [[k v] x]
+                 (aset obj (clj->js k) (clj->js v)))
+               obj)
+    (coll? x) (apply array (map clj->js x))
+    :else x))
+
+(defn render [template_name locals]
+  ( (. engine (compile (slurp (str "./views/" template_name ".html")))) (clj->js locals)))
+
+(defn slideshow [from-filename]
+  (letfn 
+    [(step-in          [ x ] (do (println x) x))
+
+     (read-slides      [from-file] (slurp from-file))
+     (split-slides     [raw-markup] (re-seq #"!SLIDE (\w+)?\s+(.*)" raw-markup))
+     (build-slides     [slide-components] (map (fn [[_ t mkp]] {:type (or t "markdown"), :content mkp}) slide-components))
+     (render-slide     [{typ :type mkp :content}] (render (str typ "_slide") {:content  mkp}))
+     (rendered-slides  [slide-maps] (map render-slide slide-maps))
+     (render-slideshow [slides] (render "index" {:slides slides}))]
+    (-> 
+      from-filename
+      read-slides
+      split-slides
+      build-slides
+      rendered-slides
+      step-in
+      render-slideshow)))
+
+(defn create-slideshow [request response]
+  (.send response (slideshow "./slides.md")))
 
 (defn -serve [& args]
   (let [port (or (.-PORT (.-env node/process)) 5000)]
     (doto app
+      ;configuration
       (.use (. express (logger)))
-      (.get "/" hello-world)
+
+      ;view rendering: didn't work, keeps whining about a callback!
+      ;(.engine "html" (.-handlebars engines))
+      ;(.set "view engine" "html")
+      ;(.set "views" (str node/__dirname "/views"))
+
+      ;routes
+      (.get "/" create-slideshow)
+
+      ;put this in your closure and func it
       (.listen port))
     (println (str "Listening on port" port))))
 
